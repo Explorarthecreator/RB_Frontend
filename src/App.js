f4b7a72f7f8aa950f30a27ead8be65fdc4f0cae0
Otti Daniel <ottidan20@gmail.com>
2021-12-07 16:06:05 +0100

    set up cra with tailwind css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-red-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to <code>RB FRONTEND</code> make changes and save to reload.
+          Goodluck
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
